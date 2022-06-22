24c23c74112d43af27fb7b024773d8547fd38ef2
prnallamudi <praveen-kumar.nallamudi@nagarro.com>
2022-06-22 14:47:28 +0530

    assignment added

---
+++ source/repos/Sample/Sample/Controllers/FirstMvcController1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Controllers
+{
+    public class FirstMvcController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Caliculator()
+        {
+            return View();
+        }
+        [HttpPost]  
+        public IActionResult Caliculator(CaliculatorModel cal)
+        {
+            int a = cal.num1;
+            int b = cal.num2;
+           
+            if (cal.caliculate == "add")
+            {
+                cal.tot = a + b;
+            }
+            else if (cal.caliculate == "sub")
+            {
+                cal.tot = a - b;
+            }
+            else if (cal.caliculate == "div")
+            {
+                cal.tot = a / b;
+            }
+            else if(cal.caliculate=="mul")
+            {
+                cal.tot = a * b;
+            }
+            else
+            {
+                cal.tot = 0;    
+            }
+            ViewData["result"] = cal.tot;
+           
+            return View();
+        }
+    }
+}
---
+++ source/repos/Sample/Sample/Models/CaliculatorModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Models
+{
+    public class CaliculatorModel
+    {
+        public int num1 { get; set; }
+
+        public int num2 { get; set; }
+
+        public int tot { get; set; }
+
+        public String caliculate { get; set; }
+    }
+}
